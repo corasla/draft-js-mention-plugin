66920023e1ec26b834e40e0b9b2a0fb4aa345b82
Razvan Milea <milearzv@gmail.com>
2016-10-24 19:52:25 +0300

    Remove max 5 results from defaultSuggestionsFilter

--- src/utils/defaultSuggestionsFilter.js
+++ src/utils/defaultSuggestionsFilter.js
@@ -1,11 +1,11 @@
-// Get the first 5 suggestions that match
+// Get all suggestions that match searchValue
 const defaultSuggestionsFilter = (searchValue, suggestions) => {
   const value = searchValue.toLowerCase();
   const filteredSuggestions = suggestions.filter((suggestion) => (
     !value || suggestion.get('name').toLowerCase().indexOf(value) > -1
   ));
-  const size = filteredSuggestions.size < 5 ? filteredSuggestions.size : 5;
-  return filteredSuggestions.setSize(size);
+
+  return filteredSuggestions.setSize(filteredSuggestions.size);
 };
 
 export default defaultSuggestionsFilter;
