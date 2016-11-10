bbff5e86069a7b83ca1ffe5a28205020d861149f
Razvan Milea <milearzv@gmail.com>
2016-11-10 10:04:03 +0200

    Implement fix, add support for some german characters

--- src/mentionSuggestionsStrategy.js
+++ src/mentionSuggestionsStrategy.js
@@ -4,7 +4,7 @@ import findWithRegex from 'find-with-regex';
 import escapeRegExp from 'lodash.escaperegexp';
 export default (trigger: String) => (contentBlock: Object, callback: Function) => {
   // const HASHTAG_REGEX = /(?:(^|[ ]))${escapeRegExp(trigger)}([a-zA-Z0-9][a-zA-Z0-9]*)(?!\S)+/g;
-  const HASHTAG_REGEX = /(?:(^|[\s]))#([a-zA-Z0-9][a-zA-Z0-9]*)(?!\S)+/g;
+  const HASHTAG_REGEX = /(?:(^|[\s]))#([a-zA-Z0-9üöäÜÖÄ][a-zA-Z0-9üöäÜÖÄ]*)(?!\S)+/g;
   findWithRegex(HASHTAG_REGEX, contentBlock, callback);
   // findWithRegex(new RegExp(`(\\s|^)${escapeRegExp(trigger)}[\\w\u4e00-\u9eff]*`, 'g'), contentBlock, callback);
 };
